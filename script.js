75232018eaa0acb4f02a78646bc3c9168afa176c
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-04-26 22:03:09 -0400

    adjusted form positioning, stylying adjustments  and removed a few css lines

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const submit = document.getElementById('submit');
 submit.addEventListener('click', function(){
       alert('Information has been submitted!')
 });
+
+const hover = document.getElementById('head1');
+hover.addEventListener("mouseover", function(){
+      hover.style.backgroundColor = '#FCAE3F';
+});
+
+const hover1 = document.getElementById('head2');
+hover1.addEventListener("mouseover", function(){
+      hover1.style.backgroundColor = '#FCAE3F';
+})
+const hover2 = document.getElementById('head3');
+hover2.addEventListener("mouseover", function(){
+      hover2.style.backgroundColor = '#FCAE3F';
+})
